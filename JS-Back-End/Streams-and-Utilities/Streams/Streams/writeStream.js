7744d8ad5cce7c106e18ad41bdfae45a5ce7c40b
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2024-03-10 11:29:41 +0200

    Create writeStream.js

---
+++ JS-Back-End/Streams-and-Utilities/Streams/Streams/writeStream.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream('./data/input.txt', {encoding: 'utf8'});
+
+for (let i = 0; i < 1000; i++) {
+    writeStream.write(`${Math.random()}\n`);
+}
+
+writeStream.end();
